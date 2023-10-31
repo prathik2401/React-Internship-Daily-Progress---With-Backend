4b86da7e599d85decc519b4fded75fe289feeaad
Sai Prathik R <saiprathik2419@gmail.com>
2023-10-31 11:55:59 +0530

    Added Image to the Web Application

--- Day4/my-app/src/App.js
+++ Day4/my-app/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+let title = "Welcome to My Application";
+  // let  person = {
+  //   name: 'John',
+  //   city: 'Bangalore',
+  //   occupation: 'Developer',
+  // }
+function Profile(props){
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <img src="https://avatars.githubusercontent.com/u/83471987?v=4" alt="Profile Pic"></img>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>{title}!</h1>
+      <h2>
+        <Profile name="Sai Prathik R"/>
+      </h2>
     </div>
   );
 }
