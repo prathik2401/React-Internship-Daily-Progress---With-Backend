13a042ea9cbc0054c8d8d65d6d2e0b46e0a02121
Sai Prathik R <saiprathik2419@gmail.com>
2023-11-14 18:12:47 +0530

    react-redux-demo

--- Day10/routing-implementation/routing-implementation/src/Header.jsx
+++ Day10/routing-implementation/routing-implementation/src/Header.jsx
@@ -3,8 +3,8 @@ import {Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-        <ul>
+    <div className='header-main'>
+        <ul className='header-list'>
             <li>
                 <Link to={'/'}>
                   Home
--- Day10/routing-implementation/routing-implementation/src/Pages/Home.jsx
+++ Day10/routing-implementation/routing-implementation/src/Pages/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
+      <div className="title-section">
         <h1>
-            Home Component
+          Home Page
         </h1>
+      </div>
+      <div className="image-section">
+        <img src="https://bst.icons8.com/wp-content/uploads/2021/06/mona-lisa-low-quality.jpg" alt="" />
+      </div>
+      <div className='details'>
+        <p>
+          Cosa come nostro forse in divenuti noi fuor non di, al maesta convenevole mossa dell'occhio purita. Giudicio i piaceri e 'l vostro. Per viviamo colui fallo coloro furono cosa che. Divenuti d'esse cospetto raccontare che in. Mentre noi che grazia che. Porgiamo oppinione che come di a. Manifestamente piú al manifesta sue in. Degli la quegli io purita e alla. Infiniti noi potendo pericoli e sí da che volta piaceri. La gli quale ripararci da', intendo sempre impetrata che cosa, da' noi in trapassare noi prieghi, come per come come di e d'esse, dio principio noi e tanto essilio cospetto in noi, d'esse lui sempre noi che sí di, infiniti nostra piú dallo grazia da cosa ancora come che. Che della niuna tanto iscacciato quali per, quale speranza tale e le nome e noi a. Essilio che le lui lui colui, occulta cospetto quella alcun procuratore potremmo sua ripararci suo modo. O sí da santo sí che cosa intendo, da che purita noi di quale, da di mentre cospetto e, essilio le cominciamento la della cose audaci senza. Mentre quale procuratori noi dio dell'occhio a noi vita. Con nel furon sí quale e di esso non. Intendo lui in alla.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
